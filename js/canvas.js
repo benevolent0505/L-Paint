103785a7dce55304caaa3a5fd94155a02c48d8c6
benevolent0505 <mikio.fjt@gmail.com>
2015-03-09 15:34:01 +0900

    brushup canvas.js

--- js/canvas.js
+++ js/canvas.js
@@ -1,69 +1,101 @@
-var canvas = document.getElementById("canvas");
-var context = canvas.getContext("2d");
-var display = document.getElementById('display');
+var LPaint = {
+    init: function() {
+    },
 
-var radius = 4;
-var drag = false;
+    paint: {
+        draw: function() {
+            console.log("draw");
+            display.innerHTML = "draw";
+        },
 
-context.lineWidth = 2 * radius;
+        erase: function() {
+            console.log("erase");
+            display.innerHTML = "erase";
+        },
 
-context.fillStyle = 'rgb(0, 0, 0)';
-context.strokeStyle = 'rgb(0, 0, 0)';
+        fill: function() {
+            console.log("fill");
+            display.innerHTML = "fill";
+        },
 
-canvas.addEventListener('mousedown', function(e) {
-    drag = true;
-});
+        selectArea: function() {
+            console.log("select-area");
+            display.innerHTML = "select area";
+        },
 
-canvas.addEventListener('mousemove', function(e) {
-    if (drag) {
-        context.lineTo(e.clientX, e.clientY);
-        context.stroke();
+        copy: function() {
+            console.log("copy");
+            display.innerHTML = "copy";
+        },
 
-        context.beginPath();
-        context.arc(e.clientX, e.clientY, radius, 0, 2 * Math.PI);
-        context.fill();
+        paste: function() {
+            console.log("paste");
+            display.innerHTML = "paste";
+        },
 
-        context.beginPath();
-        context.moveTo(e.clientX, e.clientY);
+        loadImage: function() {
+            console.log("load-image");
+            display.innerHTML = "load image";
+        }
     }
-});
+};
 
-canvas.addEventListener('mouseup', function(e) {
-    drag = false;
-    context.beginPath();
-});
+var onload_function = function() {
+    var canvas = document.getElementById("canvas");
+    var context = canvas.getContext("2d");
+    var display = document.getElementById('display');
 
-document.getElementById('draw').addEventListener('click', function(){
-    console.log("draw");
-    display.innerHTML = "draw";
-});
+    var radius = 4;
+    var drag = false;
 
-document.getElementById('erase').addEventListener('click', function(){
-    console.log("erase");
-        display.innerHTML = "erase";
-});
+    context.lineWidth = 2 * radius;
 
-document.getElementById('fill').addEventListener('click', function(){
-    console.log("fill");
-        display.innerHTML = "fill";
-});
+    context.fillStyle = 'rgb(0, 0, 0)';
+    context.strokeStyle = 'rgb(0, 0, 0)';
 
-document.getElementById('select-area').addEventListener('click', function(){
-    console.log("select-area");
-        display.innerHTML = "select area";
-});
+    canvas.addEventListener('mousedown', function(e) {
+        drag = true;
+    });
 
-document.getElementById('copy').addEventListener('click', function(){
-    console.log("copy");
-    display.innerHTML = "copy";
-});
+    canvas.addEventListener('mousemove', function(e) {
+        if (drag) {
+            context.lineTo(e.clientX, e.clientY);
+            context.stroke();
 
-document.getElementById('paste').addEventListener('click', function(){
-    console.log("paste");
-    display.innerHTML = "paste";
-});
+            context.beginPath();
+            context.arc(e.clientX, e.clientY, radius, 0, 2 * Math.PI);
+            context.fill();
 
-document.getElementById('load-image').addEventListener('click', function(){
-    console.log("load-image");
-    display.innerHTML = "load image";
-});
+            context.beginPath();
+            context.moveTo(e.clientX, e.clientY);
+        }
+    });
+
+    canvas.addEventListener('mouseup', function(e) {
+        drag = false;
+        context.beginPath();
+    });
+    document.getElementById('draw').addEventListener('click', function(){
+        LPaint.paint.draw();
+    });
+    document.getElementById('erase').addEventListener('click', function(){
+        LPaint.paint.erase();
+    });
+    document.getElementById('fill').addEventListener('click', function(){
+        LPaint.paint.fill();
+    });
+    document.getElementById('select-area').addEventListener('click', function(){
+        LPaint.paint.selectArea();
+    });
+    document.getElementById('copy').addEventListener('click', function(){
+        LPaint.paint.copy();
+    });
+    document.getElementById('paste').addEventListener('click', function(){
+        LPaint.paint.paste();
+    });
+    document.getElementById('load-image').addEventListener('click', function(){
+        LPaint.paint.loadImage();
+    });
+};
+
+document.addEventListener('DOMContentLoaded', onload_function, false);
