f761e873da08029b808d428e57e5425f4efd3c58
benevolent0505 <mikio.fjt@gmail.com>
2015-03-17 22:32:05 +0900

    edit draw.js and main.js
    線を描く仕組みをモジュール化した。

--- js/draw.js
+++ js/draw.js
@@ -1,5 +1,38 @@
 define(function(require) {
-    return function call(){
-        console.log("draw");
+    'use strict';
+
+    var canvas = init.getCanvas();
+    var context = init.getContext();
+
+    context.fillStyle = 'rgb(0, 0, 0)';
+    context.strokeStyle = 'rgb(0, 0, 0)';
+    context.lineWidth = 10;
+
+    var draw = {
+        drag: false,
+        radius: 5,
+
+        mouseDown: function() {
+            this.drag = true;
+        },
+        mouseMove: function(evt) {
+            if (this.drag) {
+                context.lineTo(evt.clientX, evt.clientY);
+                context.stroke();
+
+                context.beginPath();
+                context.arc(evt.clientX, evt.clientY, this.radius, 0, 2 * Math.PI);
+                context.fill();
+
+                context.beginPath();
+                context.moveTo(evt.clientX, evt.clientY);
+            }
+        },
+        mouseUp: function() {
+            this.drag = false;
+            context.beginPath();
+        }
     };
+
+    return draw;
 });
