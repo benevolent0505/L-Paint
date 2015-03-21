17a37b0f07d6437c6b6341b9e6b3e87748521853
Altolira <lira.soffuso69@gmail.com>
2015-03-22 01:35:05 +0900

    edit main.js
    change color

--- js/main.js
+++ js/main.js
@@ -7,7 +7,17 @@ define(function(require) {
     var copy = require('./copy');
     var paste = require('./paste');
     var loadImage = require('./loadImage');
-    var colors = require('./color');
+    var colors = document.getElementById('colors').childNodes;
+    for (var i = 0, color; color = colors[i]; i++) {
+        if (color.nodeName.toLowerCase() != 'div') continue;
+        color.addEventListener('click', function (e) {
+            var style = e.target.getAttribute('style');
+            var color = style.match(/background:(#......)/)[1];
+            context.strokeStyle = color;
+            context.fillStyle = color;
+        },false);
+    };
+
 
     var canvas = init.getCanvas();
     var context = init.getContext();
